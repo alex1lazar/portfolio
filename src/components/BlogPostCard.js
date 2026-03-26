f871b82607a90cff7bf6f6f352e4778ba997b95d
Alex Lazar <lazarva25@gmail.com>
2026-03-26 14:18:21 +0200

    blogpostcard adjustment

--- src/components/BlogPostCard.js
+++ src/components/BlogPostCard.js
@@ -5,9 +5,9 @@ const BlogPostCard = ({ title, description, link }) => (
   <Link href={link} className="block group">
     <div className="flex">
       <div className="mb-8 min-w-0">
-        <h2 className="text-text-dark mb-2 group-hover:text-text-accent group-hover:underline transition-colors">
+        <h3 className="text-text-dark mb-2 group-hover:text-text-accent group-hover:underline transition-colors">
           {title}
-        </h2>
+        </h3>
         <p className="text-text-muted line-clamp-2">
           {description}
         </p>
